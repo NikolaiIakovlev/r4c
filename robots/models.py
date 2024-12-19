a14a611142e35d2176cfecfed4e871167db4ae76
NikolaiIakovlev <118279320+NikolaiIakovlev@users.noreply.github.com>
2024-12-19 11:35:24 +0300

    robots_app

--- robots/models.py
+++ robots/models.py
@@ -1,8 +1,42 @@
 from django.db import models
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 
 class Robot(models.Model):
+    MODEL_CHOICES = [
+        ('R2', 'R2'),
+        ('13', '13'),
+        ('X5', 'X5'),
+        # ... 
+    ]
+    VERSION_CHOICES = [
+        ('D2', 'D2'),
+        ('XS', 'XS'),
+        ('LT', 'LT'),
+        # ... 
+    ]
     serial = models.CharField(max_length=5, blank=False, null=False)
     model = models.CharField(max_length=2, blank=False, null=False)
     version = models.CharField(max_length=2, blank=False, null=False)
     created = models.DateTimeField(blank=False, null=False)
+
+    def clean(self):
+        """метод валидации. Проверяем корректность введенных данных"""
+        if self.model not in [model[0] for model in self.MODEL_CHOICES]:
+            raise ValidationError("Модель не найдена.")
+        if self.version not in [version[0] for version in self.VERSION_CHOICES]:
+            raise ValidationError("Версия не найдена.")
+        if self.created > timezone.now():
+            raise ValidationError("Дата создания не может быть в будущем.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean() #вызываем валидацию перед сохранением
+        super().save(*args, **kwargs)
+
+    class Meta:
+        unique_together = ('serial', 'model', 'version')
+
+    def __str__(self):
+        return f"Robot {self.serial} {self.model}-{self.version} created {self.created}"
--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+from .models import Robot
 
-# Create your views here.
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+
+@csrf_exempt
+def create_robot(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            model = data.get('model')
+            version = data.get('version')
+            created_str = data.get('created')
+
+            #Проверка обязательных полей и формат даты
+            if not model or not version or not created_str:
+                return HttpResponseBadRequest("Missing required fields.")
+            try:
+                created = timezone.datetime.fromisoformat(created_str.replace(' ', 'T'))
+            except ValueError:
+                return HttpResponseBadRequest("Invalid date format.")
+
+            robot = Robot(model=model, version=version, created=created)
+            robot.save()  #Валидация происходит в модели Robot.save
+            return JsonResponse({'message': 'Robot created successfully.', 'id': robot.id})
+
+        except json.JSONDecodeError:
+            return HttpResponseBadRequest("Данные JSON некорректны.")
+        except ValidationError as e:
+            return HttpResponseBadRequest(str(e))
+
+    else:
+        return HttpResponseBadRequest("Метод должен быть POST.")
